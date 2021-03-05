1922b4c3d9b48bf6dcbebaf9f17c2994054d979e
rushitayade71 <rushitayade71@gmail.com>
2021-03-05 15:21:06 +0530

    Flip coin simulator number of times

---
+++ DAY6Problem/WHILEProblems/While3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "flip Coin Simulator"
+
+echo "number of times user want to flip the coin"
+read flip
+echo $flip
+countHead=0
+countTail=0
+declare -a noOfCounts
+for((i=0;i<flip;i++))
+do
+if [ $((RANDOM%2)) -eq 1 ]
+then
+((countHead++))
+else
+((countTail++))
+fi
+done
+echo "head="$countHead
+echo "tail="$countTail
+
+noOfCounts[tail]=$countTail
+noOfCounts[head]=$countHead
+
+
+
+
